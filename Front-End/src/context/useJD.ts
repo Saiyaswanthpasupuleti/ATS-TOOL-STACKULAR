77fb0196894b704b25de1ef52189b40b7a020d3a
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-04-23 15:48:03 +0530

    na

--- Front-End/src/context/useJD.ts
+++ Front-End/src/context/useJD.ts
@@ -1,8 +1,9 @@
-import { useContext } from 'react'
-import { JDContext } from './JDContext'
+import { useContext } from 'react';
+import { JDContext } from './JDContext';
 
 export function useJD() {
-  const ctx = useContext(JDContext)
-  if (!ctx) throw new Error('useJD must be used inside JDProvider')
-  return ctx
+  const ctx = useContext(JDContext);
+  if (!ctx) throw new Error('useJD must be used inside JDProvider');
+
+  return ctx;
 }
